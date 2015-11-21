fc4f99b520446a88ea1f3ca5a87a8aaf67ebed85
sourcebits-sandeep <sandeepbecs2006@gmail.com>
2015-11-21 14:41:07 +0530

    - API method to fetch image feed (path hardcoded)

--- Saltside/Saltside/Classes/API/SSTAPIService+Images.m
+++ Saltside/Saltside/Classes/API/SSTAPIService+Images.m
@@ -10,4 +10,16 @@
 
 @implementation SSTAPIService (Images)
 
+- (void) fetchImagesFeed:(SSTServiceCompletionBlock) completionBlock withTaskCompletion:(void(^)(NSURLSessionDataTask *)) taskCompletionBlock
+{
+	NSString *path = @"https://gist.githubusercontent.com/maclir/f715d78b49c3b4b3b77f/raw/8854ab2fe4cbe2a5919cea97d71b714ae5a4838d/items.json";
+	
+	[self dataTaskWithPath:path prarmeters:nil requestType:kSSTGETRequestType modelClass:[SSTImagesFeed class] completion:completionBlock taskEnqueCompletion:^(NSURLSessionDataTask *task) {
+		
+		if (taskCompletionBlock) {
+			taskCompletionBlock (task);
+		}
+	}];
+}
+
 @end
