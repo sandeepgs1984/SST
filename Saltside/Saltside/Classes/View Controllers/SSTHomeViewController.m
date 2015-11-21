11a2c90b4258928c25073ec1f46af58d7ec52c81
sourcebits-sandeep <sandeepbecs2006@gmail.com>
2015-11-21 14:45:11 +0530

    - Fetch image API in the controller and logging

--- Saltside/Saltside/Classes/View Controllers/SSTHomeViewController.m
+++ Saltside/Saltside/Classes/View Controllers/SSTHomeViewController.m
@@ -8,7 +8,15 @@
 
 #import "SSTHomeViewController.h"
 
+#import "SSTAPIService+Images.h"
+#import "SSTImagesFeed.h"
+
+#import "SSTUtilities.h"
+
 @interface SSTHomeViewController ()
+
+@property (nonatomic, strong) SSTImagesFeed *imageFeed; // holds the response
+@property (nonatomic, strong) NSURLSessionDataTask *imageTask; // used when cancelling task
 
 @end
 
@@ -17,6 +25,29 @@
 - (void)viewDidLoad
 {
 	[super viewDidLoad];
+	
+	__weak typeof(self) blockSelf = self;
+	SSTAPIService *service = [SSTAPIService sharedService];
+	[service fetchImagesFeed:^(SSTImagesFeed *result, BOOL isCached, NSError *error) {
+		
+		if (result) {
+			blockSelf.imageFeed = result;
+			
+			NSLog(@"images feed:%@", result);
+			
+				// Clear saved task when completed
+			if (!isCached) {
+				blockSelf.imageTask = nil;
+			}
+		}
+		else {
+			[SSTUtilities showAlertWithError:error];
+		}
+		
+	} withTaskCompletion:^(NSURLSessionDataTask *task) {
+		
+		blockSelf.imageTask = task;
+	}];
 }
 
 - (void)didReceiveMemoryWarning
